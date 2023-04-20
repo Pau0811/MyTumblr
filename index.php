b53174d3ed143cbbfa9bfdd8933ff0f00b4b0d0b
Pauline Luz L. Maigting <maigtingpaulineluz@gmail.com>
2023-04-20 01:14:29 -0700

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+//start the session function
+SESSION_START();
+
+
+//create a pre defined username and password since we do not have database
+$acc_username = "PaulineMaigting";
+$acc_password = "paulineluz08";
+$acc_fullname = "Pauline Luz L. Maigting";
+$acc_address = "Boac,Marinduque";
+
+//check the current url address
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+//condition to know if the button is clicked
+if(isset($_REQUEST['login_button']) ===true) {
+	//get the username and password from the form and compare to the pre defined username and password
+	if($_REQUEST['form_username'] != $acc_username) {
+		header("Location: ".$url_add."?notexists");
+	}
+	elseif ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password) {
+		header("Location: ".$url_add."?wrongpass");
+	}
+	elseif ($_REQUEST['form_username'] ==$acc_username && $_REQUEST['form_password'] ==             $acc_password) {
+		header("Location: ".$url_add."?success");
+
+
+		//create a session variables
+		$_SESSION['ses_username'] = $acc_username;
+		$_SESSION['ses_password'] = $acc_password;
+		$_SESSION['ses_fullname'] = $acc_fullname;
+		$_SESSION['ses_address'] = $acc_address;
+	}
+
+
+}//end of loginbutton
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,10 +46,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-	
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
 	<link rel="stylesheet" href="css/style.css">
 
 	</head>
@@ -24,12 +62,47 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+
+
+
+
+
+						<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php
+		      			//this is the messaging
+		      			if(isset($_REQUEST['notexists']) ===true){
+		      				echo "<div class='alert alert-danger' role='alert'> Username does not exists </div>";	
+
+		      			}elseif(isset($_REQUEST['wrongpass'])===true){
+		      				      echo "<div class='alert alert-warning' role='alert'> Incorrect Password </div>";
+
+		      			}elseif(isset($_REQUEST['success'])===true){
+		      				      echo "<div class='alert alert-success' role='alert'> Redirecting 
+		      				        </div>";
+		      				      header ("Refresh: 5; url=account.php");
+
+		      			}elseif(isset($_REQUEST['logout'])===true){
+
+                        echo "<div class='alert alert-info' role='alert'> Thank You
+		      				        </div>";
+		      		  }elseif (isset($_REQUEST['logfirst'])===true) {
+		      		          echo "<div class='alert alert-info' role='alert'> Please Login First
+		      				        </div>";
+		      		  }elseif (isset($_SESSSION['ses_'])===false) {
+		      		          echo "<div class='alert alert-warning' role='alert'> You are still looged in. Please <a href='account.php'> click here to proceed. </div>";
+
+		      			}
+
+		      			?>
+
+		      			
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password"required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,7 +116,8 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name
+	            	="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -59,4 +133,3 @@
 
 	</body>
 </html>
-
